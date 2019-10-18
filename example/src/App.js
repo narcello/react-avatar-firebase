277447e2bd422acd1c81bd6860aeb043567439fe
MarcelloVSilva <marcellovcs@gmail.com>
2019-10-17 22:51:23 -0300

    add more example components

--- example/src/App.js
+++ example/src/App.js
@@ -5,6 +5,21 @@ const App = () => {
   return (
     <div>
       <ReactAvatarFirebase />
+
+      <ReactAvatarFirebase readOnly imageSrc='https://cdn.pixabay.com/photo/2017/10/27/15/52/jaguar-2894706__340.jpg' />
+
+      <ReactAvatarFirebase
+        animationTime='0.1s'
+        imageSrc='https://cdn.pixabay.com/photo/2017/10/27/15/52/jaguar-2894706__340.jpg' />
+
+      <ReactAvatarFirebase
+        size='64px'
+        imageSrc='https://cdn.pixabay.com/photo/2017/10/27/15/52/jaguar-2894706__340.jpg' />
+
+      <ReactAvatarFirebase
+        borderColor='#c01247'
+        size='256px'
+        animationTime='0.1s' />
     </div>
   )
 }
